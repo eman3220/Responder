4b0c57c1627379f0da058039f5516c3b095be724
Emmanuel Godinez <manuel.godinez22@gmail.com>
2013-12-11 00:29:40 +1300

    subject and snatcher
    Subject class started... cleared unnecessary snatcher data

--- Responder/src/data/Subject.java
+++ Responder/src/data/Subject.java
@@ -1,5 +1,21 @@
 package data;
 
+import java.io.File;
+import java.util.ArrayList;
+
 public class Subject {
 	// this is what Eman will work on next
+	
+	private String title;
+	private ArrayList<String> bulletPoints;
+	private File subjFile;
+	
+	public Subject(File subjFile){
+		this.subjFile = subjFile;
+		readFile();
+	}
+
+	private void readFile() {
+		
+	}
 }
